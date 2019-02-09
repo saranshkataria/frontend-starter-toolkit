a90bb49219279fbaddedadcc5fe2e6c93c67e8b2
saranshkataria <saransh@wisdomgeek.com>
2019-02-09 00:57:22 -0800

    split webpack config according to environments

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const modeConfig = mode => require(`./build-utils/webpack.${mode}`)(mode);
 
-module.exports = ({ mode }) => {
-  return {
-    mode: mode,
-    plugins: [
-      new HtmlWebpackPlugin({
-        hash: true
-      }),
-      new webpack.ProgressPlugin()
-    ]
-  };
+module.exports = ({ mode } = { mode: 'production' }) => {
+  return webpackMerge(
+    {
+      mode,
+      plugins: [
+        new HtmlWebpackPlugin({
+          hash: true
+        }),
+        new webpack.ProgressPlugin()
+      ]
+    },
+    modeConfig(mode)
+  );
 };
