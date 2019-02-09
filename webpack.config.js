fab5aa1742c83fb1b9e5cad0fc873720b1ae4b0f
saranshkataria <saransh@wisdomgeek.com>
2019-02-09 00:15:28 -0800

    set HtmlWebpackPlugin property to append hash to filename to bust cache

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = ({ mode }) => {
   return {
     mode: mode,
-    plugins: [new HtmlWebpackPlugin()]
+    plugins: [
+      new HtmlWebpackPlugin({
+        hash: true
+      })
+    ]
   };
 };
