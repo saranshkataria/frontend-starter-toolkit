c70374bfd2b044641e2a3e4cd4a4dce1be2761d4
saranshkataria <saransh@wisdomgeek.com>
2019-02-16 22:28:28 -0800

    added settings to dev server to compress files & show overlay on error

--- build-utils/webpack.development.js
+++ build-utils/webpack.development.js
@@ -2,5 +2,10 @@ module.exports = () => ({
   devtool: 'eval-source-map',
   devServer: {
     open: true,
+    compress: true,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
   },
 });
