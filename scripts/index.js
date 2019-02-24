4eface91bd9fb56359bf332b1c600e7f0c163c40
saranshkataria <saransh@wisdomgeek.com>
2019-02-23 19:30:50 -0800

    updated script to write pacakge.json in in newly generated folder

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,27 @@
 #!/usr/bin/env node
 const fs = require('fs-extra');
-const folderName = process.argv.slice(2);
-fs.ensureDirSync('./' + folderName);
+const path = require('path');
+const os = require('os');
+
+let folderName = process.argv[2];
+if (folderName) {
+  folderName = folderName.toString();
+} else {
+  console.log(
+    'Please specify a directory name to install frontend starter toolkit in'
+  );
+  process.exit(1);
+}
+fs.ensureDirSync(folderName);
+
+const packageJson = {
+  name: folderName,
+  version: '0.0.1',
+};
+
+const root = path.resolve(folderName);
+
+fs.writeFileSync(
+  path.join(root, 'package.json'),
+  JSON.stringify(packageJson, undefined, 2) + os.EOL
+);
