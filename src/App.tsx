05d7770eab88af7d0cc293e44e46a541e899c30c
saranshkataria <saransh@wisdomgeek.com>
2019-03-23 08:15:17 -0700

    replaced deprecated SFC with FunctionComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { ReactElement } from 'react';
 import { hot } from 'react-hot-loader/root';
 
-const App: React.SFC<{}> = (): ReactElement => <div>Hello World</div>;
+const App: React.FunctionComponent<{}> = (): ReactElement => (
+  <div>Hello World</div>
+);
 App.displayName = 'App';
 export default hot(App);
