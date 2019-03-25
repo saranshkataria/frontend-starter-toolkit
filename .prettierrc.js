ef1726b9512c9bf54f18bc08080e3936c7785f1a
saranshkataria <saransh@wisdomgeek.com>
2019-03-24 17:17:10 -0700

    added comments to prettierrc file

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Print trailing commas wherever possible when multi-line and where valid in ES5 (objects, arrays, etc.)
+  trailingComma: 'es5',
+  // Use single quotes instead of double quotes, prettier defaults to double quotes
+  singleQuote: true,
+  // Print semicolons at the ends of statements.
+  semi: true,
+};
