2c84f90ae9c36c0d27af5e9ba5c027933a468fdc
saranshkataria <saransh@wisdomgeek.com>
2019-04-14 03:55:39 -0700

    requiring scripts in toolkit for init

--- packages/frontend-starter-scripts/scripts/init.js
+++ packages/frontend-starter-scripts/scripts/init.js
@@ -3,4 +3,10 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-fs.copySync(path.join(process.argv[1], '../..', 'src'), path.join(root, 'src'));
+const init = function() {
+  fs.copySync(
+    path.join(process.argv[1], '../..', 'src'),
+    path.join(root, 'src')
+  );
+};
+module.exports = init;
